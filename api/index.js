4d4568ddd3a91cd6aae37c56cc605b2257ab86a5
Angel-pv <angel.villamizar.p@gmail.com>
2023-03-29 21:23:00 -0500

    vercel

--- api/index.js
+++ api/index.js
@@ -23,8 +23,17 @@ const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 //alter o force
+/*
 conn.sync({ alter: true }).then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
+*/
+(async function startServer(){
+  try{
+    server.listen(port, ()=> console.log(`Server running on http://localhost:${port}. or ${VERCEL_URL}`))
+  }catch(error){
+    console.log('Unable to initiate', error)
+  }
+})(); 
